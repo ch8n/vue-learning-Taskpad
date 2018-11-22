5bcd7864754883d0fa35644c688a4b0df8405489
ch810 <chetan.gupta@u2opiamobile.com>
2018-11-23 02:26:09 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+  
+  <h1>{{title}}</h1>
+  
+  <br>
+  <h3>All task</h3>
+  <task-list />
+
+  <br>
+  <h3>Completed task</h3>
+  <task-list/>
+
+  <br>
+  <h3>Incompleted task</h3>
+  <task-list/>
+  
+  </div>
+</template>
+
+<script>
+import TaskList from "./Task";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "Welcome to TaskPad in Vue.js"
+    };
+  },
+  components: {
+    "task-list": TaskList
+  }
+};
+</script>
+
+<style>
+</style>
